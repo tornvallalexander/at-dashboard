3576b44cd4277cf5199a5fdc453689c1c5e07600
Alexander Törnvall <alexander.tornvall@icloud.com>
2022-01-14 21:49:28 +0100

    basic typing implementation

--- src/components/Typing.svelte
+++ src/components/Typing.svelte
@@ -1,4 +1,6 @@
-<script>
+<script lang="ts">
+	import { onMount } from 'svelte';
+
 	const words = [
 		"rack",
 		"taxi",
@@ -51,13 +53,111 @@
 		"introduce",
 		"dry",
 	]
+
+	onMount(() => {
+		window.document.onkeydown = (e) => handleKeystroke(e)
+	})
+
+	interface KeystrokeProps {
+		key: string;
+		altKey: boolean;
+		ctrlKey: boolean;
+		shiftKey: boolean;
+	}
+
+	const handleKeystroke = ({ key, altKey, ctrlKey, shiftKey }: KeystrokeProps) => {
+		switch (key) {
+			case "Backspace":
+				// check special keys
+				// handleBackCtrl()
+				handleBack()
+				break;
+			case " ":
+				handleSpace()
+				break;
+			case "Enter":
+				break;
+			case "Control":
+				break;
+			case "Alt":
+				break;
+			default:
+				// check special keys
+				handleChar(key)
+		}
+
+		console.log(key)
+	}
+
+	const handleChar = (key: string) => {
+		const DOMCurrentWord = getCurrentWord()
+		if (DOMCurrentWord.className.includes("underline")) {
+			return
+		}
+		const currentChar = words[current.word][current.char]
+		if (key === currentChar) {
+			const DOMCurrentChar = getCurrentChar()
+			DOMCurrentChar.classList.add("text-gray-100")
+			current.char += 1
+			return
+		}
+		DOMCurrentWord.classList.add("underline")
+	}
+
+	const handleSpace = () => {
+		current.word += 1
+		current.char = 0
+	}
+
+	const handleBack = () => {
+		if (current.char === 0) {
+			if (current.word === 0) {
+				return
+			}
+			removeWordError()
+			current.word -= 1
+			current.char = words[current.word].length
+			return
+		}
+		const DOMPrevChar = getPrevChar()
+		DOMPrevChar.classList.remove("text-gray-100")
+		current.char -= 1
+		if (DOMPrevChar.innerText === words[current.word][current.char]) {
+			removeWordError()
+		}
+	}
+
+	const getCurrentChar = () => {
+		const DOMRef = `c${current.word}${current.char}`
+		return document.getElementById(DOMRef)
+	}
+
+	const getPrevChar = () => {
+		const DOMRef = `c${current.word}${current.char - 1}`
+		return document.getElementById(DOMRef)
+	}
+
+	const getCurrentWord = () => {
+		const DOMRef = `w${current.word}`
+		return document.getElementById(DOMRef)
+	}
+
+	const removeWordError = () => {
+		const DOMCurrentWord = getCurrentWord()
+		DOMCurrentWord.classList.remove("underline")
+	}
+
+	$: current = {
+		word: 0,
+		char: 0,
+	}
 </script>
 
 <div class="flex flex-wrap">
-	{#each words as word}
-		<div class="mr-3 text-gray-500 text-2xl font-medium">
-			{#each word as char}
-				<span>
+	{#each words as word, i}
+		<div class="mr-3 text-gray-500 text-2xl font-medium" id={`w${i}`}>
+			{#each word as char, j}
+				<span id={`c${i}${j}`}>
 					{char}
 				</span>
 			{/each}
